608464f389b6c88b3716b0b729ea59cad587c420
ollymonger <olivermonger123@gmail.com>
2019-03-26 17:05:51 +0000

    Stashing changes

--- src/averageTest.ts
+++ src/averageTest.ts
@@ -2,7 +2,7 @@ export const average = (inputs: any) => {
     var sum = 0;
     for (var i = 0; i < inputs.length; i++) {
         sum += inputs[i];   
-        var result = sum / inputs.length;
     }
+    var result = sum / inputs.length;
     return result;
 }
